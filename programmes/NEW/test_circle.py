3098b3198fadb24c27a831f8aa3c0ee49c91d749
Radka Marty <martyradka@gmail.com>
2018-08-03 13:58:59 +0200

    Changes in circle tests

--- programmes/NEW/test_circle.py
+++ programmes/NEW/test_circle.py
@@ -3,14 +3,14 @@ from circle import get_circumference, get_area_of_circle
 
 def test_get_circumference():
     assert get_circumference(3.4) == 21.362830044410593
-    assert not get_circumference(3)
+    assert not get_circumference(3) != 18.84955592153876
     assert not get_circumference(0)
-    assert not get_circumference(-3)
+    assert not get_circumference(-3) != -18.84955592153876
 
 
 def test_get_area_of_circle():
     assert get_area_of_circle(1.4) == 6.157521601035993
-    assert not get_area_of_circle(1)
+    assert not get_area_of_circle(1) == 6.283185307179586
     assert not get_area_of_circle(0)
-    assert not get_area_of_circle(-1.6)
+    assert not get_area_of_circle(-1.6) == -10.053096491487338
 
--- programmes/NEW/test_circle2.py
+++ programmes/NEW/test_circle2.py
@@ -5,11 +5,11 @@ def test_get_circumference():
     assert get_circumference(3.4) == 21.362830044410593
     assert get_circumference(3) == 18.84955592153876
     assert not get_circumference(0)
-    assert not get_circumference(-3.4)
+    assert not get_circumference(-3.4) != -21.362830044410593
 
 
 def test_get_area_of_circle():
     assert get_area_of_circle(1.4) == 6.157521601035993
     assert get_area_of_circle(1) == 3.141592653589793
     assert not get_area_of_circle(0)
-    assert not get_area_of_circle(-1.4)
+    assert not get_area_of_circle(-1.4) == -6.157521601035993
